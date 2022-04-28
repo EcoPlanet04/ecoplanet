695abaf2df40cda14749cc00beaa4f2a0181ddf7
EcoPlanet04 <ecoplanetpg@gmail.com>
2022-04-27 22:15:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//teste zero
+const express = require("express");
+const app = express();
+const port = 8081
+
+app.use(express.json());
+
+    const nome = req.body.exemplo2;   
+    const idade = req.body.exemplo1;
+
+    app.listen(8081, () => {
+        console.log("Servidor iniciado na porta 8081: http://localhost:8081");
+    });
