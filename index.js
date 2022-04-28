92e91a76dfecfc486bb338ad095237f73ae259c9
EcoPlanet04 <ecoplanetpg@gmail.com>
2022-04-27 22:31:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port = 8081 // porta habilitada
 
 app.use(express.json()); // express funcional em conjunto com .json
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     let indexPath = path.join(__dirname, "./estrutura/index.html");
     res.sendFile(indexPath);
-  }) // 
+  })
 
-    const nome = req.body.exemplo2;   
-    const idade = req.body.exemplo1;
+    const exemplo1 = req.body.exemplo2;   // chamada de conteúdo na web exemplo, um nome e através da chamada, possibilita o tratamento
+    const exemplo2 = req.body.exemplo1; */ //tratar erro, não está referenciando
 
     app.listen(8081, () => {
         console.log("Servidor iniciado na porta 8081: http://localhost:8081");
